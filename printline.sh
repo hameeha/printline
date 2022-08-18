d385f8d7658555f8ecc0835875f081055a0fcabd
haris <harishameed00@gmail.com>
2022-08-18 12:39:14 +0000

    basic mode printline.sh

---
+++ printline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#program to print lines in a file
+print_line()
+{
+       
+
+echo "Corresponding line is:"
+sed -n "${1}p" $filename
+        
+
+}
+
+echo "Please enter the filename"
+read filename
+echo "enter the line numer to print"
+read linenumber
+print_line $linenumber
+
